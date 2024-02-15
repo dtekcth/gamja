7e5e94cda0c449519dfdd6996c0b2e3c83df25df
Simon Ser <contact@emersion.fr>
2024-02-15 16:37:32 +0100

    components/help: always show autocomplete keybind

--- components/help.js
+++ components/help.js
@@ -26,11 +26,13 @@ function KeyBindingsHelp() {
 		`;
 	});
 
+	l.push(html`
+		<dt><kbd>Tab</kbd></dt>
+		<dd>Automatically complete nickname or channel</dd>
+	`);
+
 	if (!window.matchMedia("(pointer: none)").matches) {
 		l.push(html`
-			<dt><kbd>Tab</kbd></dt>
-			<dd>Automatically complete nickname or channel</dd>
-
 			<dt><strong>Middle mouse click</strong></dt>
 			<dd>Close buffer</dd>
 		`);
