7c445d0bc9e535e4143b69a3519d089c4705dd60
Simon Ser <contact@emersion.fr>
2024-09-28 21:45:45 +0200

    Add ESLint

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import globals from "globals";
+import js from "@eslint/js";
+
+export default [
+	js.configs.recommended,
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				"process": "readonly",
+			},
+		},
+		rules: {
+			"no-case-declarations": "off",
+			"no-unused-vars": ["error", {
+				args: "none",
+				caughtErrorsIgnorePattern: "^_",
+				destructuredArrayIgnorePattern: "^_",
+			}],
+		},
+	},
+];
